20f6ee11a93664e3af8cccc62ce848f1dd4a231e
stringmanolo <manuelvarelacaldas@gmail.com>
2021-11-26 16:00:59 +0100

    Express Server

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,5 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const port = 3000;
 const exit = (exitMsg) => {
     console.error(exitMsg);
     process.exit(-1);
@@ -10,3 +17,15 @@ const config = {
     password: "temp",
     port: 5432
 };
+/* <main> */
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.get("/", (request, response) => {
+    response.json({
+        info: "SNR API"
+    });
+});
+app.listen(port, () => {
+    console.log("Server binded to localhost:" + port);
+});
+/* </main> */
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import { Client } from "pg";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
 
 const exit = (exitMsg: string) => {
   console.error(exitMsg);
@@ -13,3 +18,18 @@ const config = {
 };
 
 
+
+/* <main> */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.json({
+    info: "SNR API"
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server binded to localhost:" + port);
+});
+/* </main> */
